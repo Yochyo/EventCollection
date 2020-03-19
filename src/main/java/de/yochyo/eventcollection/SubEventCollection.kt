84f6acd393970096f179a1cbd1b5ea842d5abebe
Yochyo <herodragon25@gmail.com>
2020-03-19 15:28:59 +0100

    moved out remove/add method to seperate method

--- src/main/java/de/yochyo/eventcollection/SubEventCollection.kt
+++ src/main/java/de/yochyo/eventcollection/SubEventCollection.kt
@@ -5,7 +5,8 @@ import de.yochyo.eventcollection.events.OnClearEvent
 import de.yochyo.eventcollection.events.OnRemoveElementEvent
 import de.yochyo.eventmanager.Listener
 import java.util.function.Predicate
-class SubEventCollection<T>(c: MutableCollection<T>, val parentCollection: EventCollection<T>, filter: (e: T) -> Boolean) : EventCollection<T>(c) {
+
+open class SubEventCollection<T>(private val c: MutableCollection<T>, val parentCollection: EventCollection<T>, filter: (e: T) -> Boolean) : EventCollection<T>(c) {
     override fun add(element: T) = parentCollection.add(element)
     override fun addAll(elements: Collection<T>) = parentCollection.addAll(elements)
     override fun remove(element: T) = parentCollection.remove(element)
@@ -22,19 +23,23 @@ class SubEventCollection<T>(c: MutableCollection<T>, val parentCollection: Event
     }
     private val onAddElementParent = object : Listener<OnAddElementEvent<T>>() {
         override fun onEvent(e: OnAddElementEvent<T>) {
-            if (filter(e.element)) {
-                c.add(e.element)
-                onAddElement.trigger(OnAddElementEvent(c, e.element))
-            }
+            if (filter(e.element)) addToCollection(e.element)
         }
     }
     private val onRemoveElementParent = object : Listener<OnRemoveElementEvent<T>>() {
         override fun onEvent(e: OnRemoveElementEvent<T>) {
-            if (filter(e.element)) {
-                c.remove(e.element)
-                onRemoveElement.trigger(OnRemoveElementEvent(c, e.element))
-            }
+            if (filter(e.element)) removeFromCollection(e.element)
         }
+    }
+
+    protected fun addToCollection(element: T) {
+        c.add(element)
+        onAddElement.trigger(OnAddElementEvent(c, element))
+    }
+
+    protected fun removeFromCollection(element: T) {
+        c.remove(element)
+        onRemoveElement.trigger(OnRemoveElementEvent(c, element))
     }
 
     init {
